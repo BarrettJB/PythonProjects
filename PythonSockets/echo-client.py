a0f8e2e907001d4f1290aadc44809101b7662ff3
Barrett Bryson <bbryson1@lenovo.com>
2019-03-27 11:26:06 -0400

    Started a socket communication project

---
+++ PythonSockets/echo-client.py
@@ -0,0 +1,16 @@
+import socket
+
+HOST = '127.0.0.1'
+#HOST = '98.26.116.113'
+PORT = 3656
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+  s.connect((HOST, PORT))
+  print("Connected to host ('exit' to leave)")
+  shouldContinue = True
+  while(shouldContinue):
+    msg = input()
+    s.sendall(msg.encode('utf-8'))
+    data = s.recv(1024)
+
+print('Recived: ', repr(data))
---
+++ PythonSockets/echo-server.py
@@ -0,0 +1,21 @@
+import socket
+
+HOST = '127.0.0.1'
+#HOST = '98.26.116.113'
+PORT = 3656
+
+while True:
+  with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST,PORT))
+    print('Listening for connection')
+    s.listen()
+    conn, addr = s.accept()
+    with conn:
+      print('Connected by', addr)
+      while True:
+        data = conn.recv(1024)
+        if not data:
+          print('Client disconnected...')
+          break
+        print(data)
+        conn.sendall(data)
